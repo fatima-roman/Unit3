623f7f921ae67f97fda9e18a62e9018b43753683
fatima-roman <fatima.roman@iesnervion.es>
2025-12-01 10:26:22 +0100

    ej2

---
+++ src/boletin1/EJ02.java
@@ -0,0 +1,25 @@
+package boletin1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EJ02 {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Inicializar la lista con un tamaño de 5 
+        double lista[] = new double[5];
+
+        for (int i =1; i<= lista.length; i++) {
+        	System.out.println("Introduce el número "+ i+ ":");
+        	lista[i-1]=sc.nextDouble();
+        }
+
+        // Imprimir lista
+        System.out.println("Números aleatorios: " + Arrays.toString(lista));
+        
+        sc.close();
+	}
+
+}
